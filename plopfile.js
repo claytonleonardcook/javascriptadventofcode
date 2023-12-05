0f2d92cd1afda4d7dfd811beddc1a845856b0edb
claytonleonardcook <claytonleonardcook@gmail.com>
2023-12-05 13:35:10 -0500

    init

---
+++ plopfile.js
@@ -0,0 +1,31 @@
+export default function (plop) {
+    plop.setGenerator('Create Day', {
+        description: 'Creates folder and files for starting a Advent of Code day.',
+        prompts: [{
+            type: 'number',
+            name: 'day',
+            message: 'What day are starting?'
+        }],
+        actions: [{
+            type: 'add',
+            path: 'src/day{{day}}/index.test.ts',
+            templateFile: 'templates/test.hbs'
+        }, {
+            type: 'add',
+            path: 'src/day{{day}}/index.ts',
+            templateFile: 'templates/code.hbs'
+        }, {
+            type: 'add',
+            path: 'src/day{{day}}/part1example.txt'
+        }, {
+            type: 'add',
+            path: 'src/day{{day}}/part1.txt'
+        }, {
+            type: 'add',
+            path: 'src/day{{day}}/part2example.txt'
+        }, {
+            type: 'add',
+            path: 'src/day{{day}}/part2.txt'
+        }]
+    });
+};
